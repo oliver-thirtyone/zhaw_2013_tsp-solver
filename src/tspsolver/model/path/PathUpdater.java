2bcbb0977771b177b11ef7e871f6d43b02d9fbf8
Oliver Streuli <oliver.streuli@bluewin.ch>
2013-05-03 15:47:05 +0200

    Forgot the PathUpdater

---
+++ src/tspsolver/model/path/PathUpdater.java
@@ -0,0 +1,105 @@
+package tspsolver.model.path;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+import tspsolver.model.grid.Edge;
+import tspsolver.model.grid.updates.UpdateAction;
+
+public class PathUpdater {
+
+	private final Path path;
+
+	private final List<Edge> edgesToAdd;
+	private final List<Edge> edgesToRemove;
+
+	private final List<Edge> addedEdges;
+	private final List<Edge> removedEdges;
+
+	public PathUpdater(Path path) {
+		this.path = path;
+
+		this.edgesToAdd = new LinkedList<Edge>();
+		this.edgesToRemove = new LinkedList<Edge>();
+
+		this.addedEdges = new LinkedList<Edge>();
+		this.removedEdges = new LinkedList<Edge>();
+	}
+
+	public synchronized void updatePath() {
+		// Mark the added edges as a path element
+		for (Edge edge : this.addedEdges) {
+			this.path.firePathUpdate(edge, UpdateAction.PATH_ELEMENT);
+		}
+		this.addedEdges.clear();
+
+		// Mark the removed edges as a normal edge
+		for (Edge edge : this.removedEdges) {
+			this.path.firePathUpdate(edge, UpdateAction.NON_PATH_ELEMENT);
+		}
+		this.removedEdges.clear();
+
+		// Add the new edges and mark them as a new path element
+		for (Edge edge : this.edgesToAdd) {
+			this.path.addEdge(edge);
+			this.path.firePathUpdate(edge, UpdateAction.NEW_PATH_ELEMENT);
+		}
+		this.addedEdges.addAll(this.edgesToAdd);
+		this.edgesToAdd.clear();
+
+		// Remove the old edges and mark them as a old path element
+		for (Edge edge : this.edgesToRemove) {
+			this.path.removeEdge(edge);
+			this.path.firePathUpdate(edge, UpdateAction.OLD_PATH_ELEMENT);
+		}
+		this.removedEdges.addAll(this.edgesToRemove);
+		this.edgesToRemove.clear();
+	}
+
+	public synchronized void addEdge(Edge edge) {
+		if (this.edgesToRemove.contains(edge)) {
+			this.edgesToRemove.remove(edge);
+		}
+		else if (!this.edgesToAdd.contains(edge) && !this.path.containsEdge(edge)) {
+			this.edgesToAdd.add(edge);
+		}
+	}
+
+	public synchronized void removeEdge(Edge edge) {
+		if (this.edgesToAdd.contains(edge)) {
+			this.edgesToAdd.remove(edge);
+		}
+		else if (!this.edgesToRemove.contains(edge) && this.path.containsEdge(edge)) {
+			this.edgesToRemove.add(edge);
+		}
+	}
+
+	public synchronized void addEdges(Set<Edge> edges) {
+		for (Edge edge : edges) {
+			this.addEdge(edge);
+		}
+	}
+
+	public synchronized void removeEdges(Set<Edge> edges) {
+		for (Edge edge : edges) {
+			this.removeEdge(edge);
+		}
+	}
+
+	public synchronized void addPath(Path path) {
+		this.addEdges(path.getEdges());
+	}
+
+	public synchronized void removePath(Path path) {
+		this.removeEdges(path.getEdges());
+	}
+
+	public synchronized void clearWholePath() {
+		this.removePath(this.path);
+	}
+
+	public final Path getPath() {
+		return this.path;
+	}
+}
