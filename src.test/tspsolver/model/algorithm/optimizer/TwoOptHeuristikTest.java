380faab411dd5a0e2c018503d66d449e48a8caf2
Oliver Streuli <oliver.streuli@bluewin.ch>
2013-05-05 16:39:40 +0200

    TwoOptHeuristikTest test class

---
+++ src.test/tspsolver/model/algorithm/optimizer/TwoOptHeuristikTest.java
@@ -0,0 +1,98 @@
+package tspsolver.model.algorithm.optimizer;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import tspsolver.model.Scenario;
+import tspsolver.model.grid.Edge;
+import tspsolver.model.grid.Grid;
+import tspsolver.model.grid.GridFactory;
+import tspsolver.model.grid.Node;
+import tspsolver.model.path.Path;
+import tspsolver.model.path.PathUpdater;
+
+public class TwoOptHeuristikTest {
+
+	private Scenario scenario;
+	private Grid grid;
+	private Path path;
+
+	private Node nodeNorth;
+	private Node nodeEast;
+	private Node nodeSouth;
+	private Node nodeWest;
+
+	private Edge edgeNorthEast;
+	private Edge edgeNorthSouth;
+	private Edge edgeNorthWest;
+	private Edge edgeEastSouth;
+	private Edge edgeEastWest;
+	private Edge edgeSouthWest;
+
+	private AOptimizerAlgorithm algorithm;
+
+	@Before
+	public void setUp() {
+		this.scenario = new Scenario("TwoOptHeuristikTest");
+		this.grid = scenario.getGrid();
+		this.path = scenario.getPath();
+
+		this.nodeNorth = GridFactory.createNode(3, 1);
+		this.nodeEast = GridFactory.createNode(4, 2);
+		this.nodeSouth = GridFactory.createNode(1, 3);
+		this.nodeWest = GridFactory.createNode(2, 4);
+
+		this.grid.addNode(this.nodeNorth);
+		this.grid.addNode(this.nodeEast);
+		this.grid.addNode(this.nodeSouth);
+		this.grid.addNode(this.nodeWest);
+		this.scenario.setStartingNode(this.nodeNorth);
+
+		this.edgeNorthEast = GridFactory.getEdge(this.nodeNorth, this.nodeEast);
+		this.edgeNorthSouth = GridFactory.getEdge(this.nodeNorth, this.nodeSouth);
+		this.edgeNorthWest = GridFactory.getEdge(this.nodeNorth, this.nodeWest);
+		this.edgeEastSouth = GridFactory.getEdge(this.nodeEast, this.nodeSouth);
+		this.edgeEastWest = GridFactory.getEdge(this.nodeEast, this.nodeWest);
+		this.edgeSouthWest = GridFactory.getEdge(this.nodeSouth, this.nodeWest);
+
+		this.algorithm = new LinKernighanHeuristik(this.scenario);
+	}
+
+	@Test
+	public void test() {
+		// Create a valid path that is too heavy
+		PathUpdater pathUpdater = new PathUpdater(this.path);
+		pathUpdater.addEdge(this.edgeNorthSouth);
+		pathUpdater.addEdge(this.edgeSouthWest);
+		pathUpdater.addEdge(this.edgeEastWest);
+		pathUpdater.addEdge(this.edgeNorthEast);
+		pathUpdater.updatePath();
+
+		// Make sure that we can not take a step yet
+		Assert.assertFalse(this.algorithm.step());
+
+		// Validate the arguments
+		this.algorithm.validateArguments();
+		Assert.assertTrue(this.algorithm.hasValidArguments());
+
+		// Step through the algorithm
+		do {
+			Assert.assertTrue(this.algorithm.step());
+		} while (!this.algorithm.hasFinishedSuccessfully());
+
+		// Check if we have four edges
+		Assert.assertEquals(4, this.path.getNumberOfEdges());
+
+		// Check if we went the right path
+		Assert.assertTrue(this.path.containsEdge(this.edgeNorthEast));
+		Assert.assertTrue(this.path.containsEdge(this.edgeEastSouth));
+		Assert.assertTrue(this.path.containsEdge(this.edgeSouthWest));
+		Assert.assertTrue(this.path.containsEdge(this.edgeNorthWest));
+
+		// Check if these edges are not part of the path
+		Assert.assertFalse(this.path.containsEdge(this.edgeNorthSouth));
+		Assert.assertFalse(this.path.containsEdge(this.edgeEastWest));
+	}
+}
