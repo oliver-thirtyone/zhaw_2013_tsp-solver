82d6479c56fa228f7f668623d97ef9e232dc41ae
Markus Peloso <markus.peloso@gmail.com>
2013-05-06 21:02:18 +0200

    Reimplement the Two Opt.
    It works.

--- src/tspsolver/model/algorithm/optimizer/TwoOptHeuristik.java
+++ src/tspsolver/model/algorithm/optimizer/TwoOptHeuristik.java
@@ -1,103 +1,142 @@
 package tspsolver.model.algorithm.optimizer;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Vector;
 
 import tspsolver.model.algorithm.OptimizerAlgorithm;
 import tspsolver.model.scenario.grid.Edge;
+import tspsolver.model.scenario.grid.Node;
 import tspsolver.model.scenario.path.Path;
 import tspsolver.model.scenario.path.PathUpdater;
 
 public class TwoOptHeuristik extends OptimizerAlgorithm {
 
-	private final List<Edge> orderlyPath;
-
-	private int i;
-	private int k;
+	private Vector<Node> nodesInOrder;
 
 	public TwoOptHeuristik() {
-		this.orderlyPath = new Vector<Edge>();
-		this.reset();
 	}
 
 	@Override
 	protected void doInitialize() {
 
+		this.nodesInOrder = new Vector<Node>();
+
+		// Get the node in line
+		Node currentNode = this.getStartingNode();
+
+		this.nodesInOrder.add(currentNode);
+
+		int numberOfEdges = this.getPath().getNumberOfEdges();
+
+		HashSet<Edge> edges = new HashSet<Edge>();
 		for (Edge edge : this.getPath().getEdges()) {
-			this.orderlyPath.add(edge);
+			edges.add(edge);
 		}
+
+		while (this.nodesInOrder.size() < numberOfEdges) {
+			for (Edge edge : edges) {
+				if (edge.getFirstNode() == currentNode) {
+
+					edges.remove(edge);
+
+					currentNode = edge.getSecondNode();
+
+					this.nodesInOrder.add(currentNode);
+					break;
+				} else if (edge.getSecondNode() == currentNode) {
+
+					edges.remove(edge);
+
+					currentNode = edge.getFirstNode();
+
+					this.nodesInOrder.add(currentNode);
+					break;
+				}
+			}
+
+		}
+
 	}
 
 	@Override
 	protected void doReset() {
-		this.orderlyPath.clear();
-
-		this.i = 0;
-		this.k = 1;
 	}
 
 	@Override
 	protected boolean doStep() {
-		if (i < this.getPath().getNumberOfEdges()) {
-			// Create the new path
-			List<Edge> newOrderlyPath = this.createNewOrderlyPath();
-			Path newPath = this.createNewPath(newOrderlyPath);
+		try {
 
-			// Check if the new path is lighter
-			if (newPath.getWeight() < this.getPath().getWeight()) {
-				this.getPathUpdater().clearPath();
-				this.getPathUpdater().addPath(newPath);
-				this.getPathUpdater().updatePath();
+			int numberOfNodes = this.getPath().getNumberOfEdges();
 
-				this.orderlyPath.clear();
-				this.orderlyPath.addAll(newOrderlyPath);
+			double lightestWeight = this.getPath().getWeight();
+
+			for (int i = 0; i < numberOfNodes; i++) {
+				for (int k = i + 1; k < numberOfNodes - 1; k++) {
+
+					Path newPath = this.generateNewPath(i, k);
+
+					double newWeight = newPath.getWeight();
+
+					if (newWeight < lightestWeight) {
+
+						this.getPathUpdater().clearPath();
+						this.getPathUpdater().addPath(newPath);
+						this.getPathUpdater().updatePath();
+						return true;
+					}
+				}
 			}
 
-			if (this.k < this.getPath().getNumberOfEdges() - 1) {
-				this.k++;
-			} else {
-				this.i++;
-				this.k = this.i + 1;
-			}
-		} else {
 			this.finishedSuccessfully();
-		}
+			return true;
 
-		return true;
+		} catch (IllegalStateException ex) {
+			return false;
+		}
 	}
 
-	// 2optSwap(route, i, k) {
-	// 1. take route[0] to route[i-1] and add them in order to new_route
-	// 2. take route[i] to route[k] and add them in reverse order to new_route
-	// 3. take route[k+1] to end and add them in order to new_route
-	// return new_route;
-	// }
-	private List<Edge> createNewOrderlyPath() {
-		List<Edge> newOrderlyPath = new Vector<Edge>();
+	private Path generateNewPath(int i, int k) {
 
-		for (int x = 0; x < this.i; x++) {
-			newOrderlyPath.add(this.orderlyPath.get(x));
+		Vector<Node> newNodePath = new Vector<Node>();
+
+		for (int j = 0; j < i; j++) {
+			newNodePath.add(this.nodesInOrder.get(j));
 		}
 
-		for (int x = this.k; x >= this.i; x--) {
-			newOrderlyPath.add(this.orderlyPath.get(x));
+		for (int j = k; j >= i; j--) {
+			newNodePath.add(this.nodesInOrder.get(j));
 		}
 
-		for (int x = this.k + 1; x < this.getPath().getNumberOfEdges(); x++) {
-			newOrderlyPath.add(this.orderlyPath.get(x));
+		for (int j = k + 1; j < this.nodesInOrder.size(); j++) {
+			newNodePath.add(this.nodesInOrder.get(j));
 		}
 
-		return newOrderlyPath;
+		return convertNodesToPath(newNodePath);
 	}
 
-	private Path createNewPath(List<Edge> edges) {
-		Path newPath = new Path();
-		PathUpdater newPathUpdater = new PathUpdater(newPath);
+	private Path convertNodesToPath(Vector<Node> nodePath) {
 
-		newPathUpdater.addEdges(edges);
-		newPathUpdater.updatePath();
+		Path path = new Path();
+		PathUpdater pathUpdater = new PathUpdater(path);
 
-		return newPath;
+		for (int j = 1; j <= this.nodesInOrder.size(); j++) {
+			Node firstNode = nodePath.get(j - 1);
+			Node secondNode = (j == this.nodesInOrder.size()) ? nodePath.get(0)
+					: nodePath.get(j);
+
+			Edge edge = firstNode.getEdgeToNode(secondNode);
+			if (edge == null) {
+				// FIXME: this path does not work, what do we do now?
+				throw new IllegalStateException();
+			}
+
+			pathUpdater.addEdge(edge);
+		}
+
+		pathUpdater.updatePath();
+
+		return pathUpdater.getPath();
 	}
 
 }
